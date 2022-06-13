e4f12cdf20bd6bbbf02d558daf99fd10ff8df974
Gopika Krishnan <gk1656@nyu.edu>
2022-06-13 11:59:07 +0400

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,3 +188,32 @@
 			});
 
 })(jQuery);
+
+// var mobile = document.createElement('div');
+// mobile.className = 'sidebar-mobile';
+// document.querySelector('.sidebar').appendChild(mobile);
+
+// // hasClass
+// function hasClass(elem, className) {
+// 	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+//   }
+  
+//   // toggleClass
+//   function toggleClass(elem, className) {
+// 	var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+// 	  if (hasClass(elem, className)) {
+// 		  while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+// 			  newClass = newClass.replace(' ' + className + ' ', ' ');
+// 		  }
+// 		  elem.className = newClass.replace(/^\s+|\s+$/g, '');
+// 	  } else {
+// 		  elem.className += ' ' + className;
+// 	  }
+//   }
+
+//   var mobileNav = document.querySelector('.sidebar-mobile');
+// var toggle = document.querySelector('.sidebar);
+// mobileNav.onclick = function() {
+//   toggleClass(this, 'sidebar-mobile-open');
+//   toggleClass(toggle, 'sidebar-active');
+// }
