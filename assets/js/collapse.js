9ce3788f227ae935e51e619384ec3cde8aa023d4
Gopika Krishnan <gk1656@nyu.edu>
2025-01-02 16:55:51 +0400

    2024 updates

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -2,13 +2,15 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+
+        // Toggle the display between 'none' and 'block'
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
